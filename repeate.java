6f866187f20df8e75e899589a9a32721a57fbacf
akashagarwal123 <akashaga420@gmail.com>
2018-10-09 18:01:36 +0530

    Successfull

---
+++ repeate.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args)throws IOException 
+	{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int num1 = Integer.parseInt(br.readLine());//enter the range
+	for(int i=0;i<num1;i++)
+	{
+	    System.out.println("Hello");
+	}
+	
+	}
+}
