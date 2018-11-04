e6c09f8eebe890862391ce1fbca6b09bdacae475
akashagarwal123 <akashaga420@gmail.com>
2018-11-04 17:29:43 +0530

    armstrong

---
+++ interval.java
@@ -0,0 +1,47 @@
+ import java.util.*;
+ class Main
+
+    {
+
+        public static void main(String[] args) 
+
+         {
+             Scanner sc = new Scanner(System.in);
+
+            int n, count = 0, a, b, c, sum = 0,start,end;
+            
+             start = sc.nextInt();
+             end = sc.nextInt();
+            for(int i = start; i <= end; i++)
+
+            {
+
+                n = i;
+
+                while(n > 0)
+
+                {
+
+                    b = n % 10;
+
+                    sum = sum + (b * b * b);
+
+                    n = n / 10;
+
+                }
+
+                if(sum == i)
+
+                {
+
+                    System.out.print(i+" ");
+
+                }
+
+                sum = 0;
+
+            }
+
+        }
+
+    }
