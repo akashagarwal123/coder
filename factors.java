7a247d61f7755d91a7e820982197c6f344b4e1f8
akashagarwal123 <akashaga420@gmail.com>
2019-03-05 19:54:27 +0530

    Factors of a no.

---
+++ factors.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        akash obj = new akash();
+        obj.cal(n);
+    }
+}
+class akash
+{
+    public void cal(int n)
+    {
+       for(int i=1;i<=n;i++)
+       {
+           if(n%i==0)
+           {
+               System.out.print(i+" ");
+           }
+       }
+    }
+}
