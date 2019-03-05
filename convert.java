fbed40f08237b19bbf558c448eb09ed6a2fd4721
akashagarwal123 <akashaga420@gmail.com>
2019-03-05 18:58:10 +0530

    Convert to Integer

---
+++ convert.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        float n = sc.nextFloat();
+        double a = Math.ceil(n);
+        System.out.println((int)a);
+    }
+}
